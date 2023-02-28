30b7b71d74a6efa8706fe9af113cf2376166febb
Stefan <iosifs617@gmail.com>
2023-02-28 21:39:26 +0200

    main

--- src/components/Important/Navbar.js
+++ src/components/Important/Navbar.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <div className='nav-left'>Test</div>
+      <div className='nav-left'>
+        <div className='nav-logo'></div>
+        <div className='nav-left-btn'>
+          <div className='nav-left-type'>Barbati</div>
+          <div className='nav-drop'>
+            <div className='nav-collumn'>
+              <div className='nav-collumn-title'><NavLink to='/'>Tricouri</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Tricouri</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Bluze</NavLink></div>
+            </div>
+            <div className='nav-collumn'>
+              <div className='nav-collumn-title'><NavLink to='/'>Pantaloni</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Scurti</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Lungi</NavLink></div>
+            </div>
+            <div className='nav-collumn'>
+              <div className='nav-collumn-title'><NavLink to='/'>Incaltaminte</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Adidasi</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Papuci</NavLink></div>
+            </div>
+          </div>
+        </div>
+        <div className='nav-left-btn'>
+          <div className='nav-left-type'>Femei</div>
+          <div className='nav-drop'>
+            <div className='nav-collumn'>
+              <div className='nav-collumn-title'><NavLink to='/'>Tricouri</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Tricouri</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Bluze</NavLink></div>
+            </div>
+            <div className='nav-collumn'>
+              <div className='nav-collumn-title'><NavLink to='/'>Pantaloni</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Scurti</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Lungi</NavLink></div>
+            </div>
+            <div className='nav-collumn'>
+              <div className='nav-collumn-title'><NavLink to='/'>Incaltaminte</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Adidasi</NavLink></div>
+              <div className='nav-collumn-prod'><NavLink to='/'>Papuci</NavLink></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className='nav-right'></div>
     </div>
   )
